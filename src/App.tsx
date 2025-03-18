5484d4cdc382f64048cb28108343c4281491142f
Guilherme <guilhermersouza34@gmail.com>
2025-03-18 17:10:29 -0300

    fazendo algumas alterações no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { BusProvider, useBus } from './contexts/BusContext';
 
 // Fix for default marker icons
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -11,9 +12,10 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const App: React.FC = () => {
+const MapComponent: React.FC = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
   const sãoPauloPosition: [number, number] = [-23.5505, -46.6333];
+  const { buses, selectedBus, setSelectedBus } = useBus();
 
   return (
     <div style={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column' }}>
@@ -45,14 +47,43 @@ const App: React.FC = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={sãoPauloPosition}>
-            <Popup>
-              São Paulo
-            </Popup>
-          </Marker>
+          {buses.map((bus) => (
+            <Marker 
+              key={bus.id} 
+              position={[bus.currentLocation.latitude, bus.currentLocation.longitude]}
+              eventHandlers={{
+                click: () => setSelectedBus(bus)
+              }}
+            >
+              <Popup>
+                <div>
+                  <h3>{bus.name}</h3>
+                  <p>Linha: {bus.number}</p>
+                  {selectedBus?.id === bus.id && (
+                    <div>
+                      <h4>Paradas:</h4>
+                      <ul>
+                        {bus.stops.map(stop => (
+                          <li key={stop.id}>{stop.name}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          ))}
         </MapContainer>
       </div>
     </div>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <BusProvider>
+      <MapComponent />
+    </BusProvider>
   );
 };
 
