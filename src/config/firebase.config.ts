ee291b674ed60a8949a72f57197c2b985b9ef123
Guilherme <guilhermersouza34@gmail.com>
2025-03-17 23:02:37 -0300

    Construção do zero

--- src/config/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "sua-api-key",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "seu-messaging-id",
-  appId: "seu-app-id"
-}; 
--- src/index.web.ts
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App.web.tsx';
-
-AppRegistry.registerComponent('BusTrackerApp', () => App);
-AppRegistry.runApplication('BusTrackerApp', {
-  rootTag: document.getElementById('root')
-});
