ee0c71d385bda4c8b64143f1e641416bd25621b3
Guilherme <guilhermersouza34@gmail.com>
2025-03-17 22:48:33 -0300

    alterações no projeto

--- webpack.config.js
+++ webpack.config.js
@@ -38,44 +38,13 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.js$/,
-        include: /node_modules[\\/]react-native-/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.ttf$/,
-        loader: 'url-loader',
-        include: /node_modules\/react-native-vector-icons/
-      }
+
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
     alias: {
-      'react-native$': 'react-native-web',
-      'react-native-gesture-handler': 'react-native-web',
-      'react-native-screens': 'react-native-web',
-      'react-native-safe-area-context': 'react-native-web/dist/modules/SafeAreaContext',
-      'react-native-safe-area-context/lib/module/SafeAreaProvider': 'react-native-web/dist/modules/SafeAreaContext',
-      'react-native-safe-area-context/lib/module/SafeAreaInsetsContext': 'react-native-web/dist/modules/SafeAreaContext',
-      'react-native-safe-area-context/lib/module/initialWindowMetrics': 'react-native-web/dist/modules/SafeAreaContext',
-      'react-native-maps': 'react-native-web-maps',
-      '@react-native-maps/maps': 'react-native-web-maps',
-      'react-native-web-maps': { MapView: 'react-native-web-maps', Marker: 'react-native-web-maps/marker' },
-      'process': 'process/browser.js',
-      '@react-navigation/elements': '@react-navigation/elements/lib/module/index.web'
-      '@react-navigation/elements': '@react-navigation/elements/lib/module/index.native',
-      'react-native-maps': 'react-native-web-maps',
-      '@react-native-firebase/app$': '@react-native-firebase/app/lib/module',
-      'react-native-reanimated': 'react-native-web',
-      'react-native-vector-icons': 'react-native-vector-icons/dist',
-      'react-native-web-maps': 'react-native-web-maps'
+      'process': 'process/browser.js'
     },
     modules: ['node_modules'],
     fallback: {
