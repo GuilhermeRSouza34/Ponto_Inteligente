a92d732dd1e5b1380566efa805275a2dced47143
Guilherme <guilhermersouza34@gmail.com>
2025-03-17 22:56:39 -0300

    alterações no projeto

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,4 +1,4 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { HomeScreen } from '../screens/HomeScreen';
@@ -6,44 +6,31 @@ import { LoginScreen } from '../screens/LoginScreen';
 import { RegisterScreen } from '../screens/RegisterScreen';
 import { RouteDetailsScreen } from '../screens/RouteDetailsScreen';
 import { SearchScreen } from '../screens/SearchScreen';
+import { MapScreen } from '../screens/MapScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
   const { user } = useAuth();
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false
+      }}
+      initialRouteName="Map"
+    >
       {!user ? (
         <>
-          <Stack.Screen 
-            name="Login" 
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen 
-            name="Register" 
-            component={RegisterScreen}
-            options={{ headerShown: false }}
-          />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
         </>
       ) : (
         <>
-          <Stack.Screen 
-            name="Home" 
-            component={HomeScreen}
-            options={{ title: 'Ponto de Ônibus Inteligente' }}
-          />
-          <Stack.Screen
-            name="Search"
-            component={SearchScreen}
-            options={{ title: 'Buscar Linha' }}
-          />
-          <Stack.Screen
-            name="RouteDetails"
-            component={RouteDetailsScreen}
-            options={{ title: 'Detalhes da Linha' }}
-          />
+          <Stack.Screen name="Map" component={MapScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Search" component={SearchScreen} />
+          <Stack.Screen name="RouteDetails" component={RouteDetailsScreen} />
         </>
       )}
     </Stack.Navigator>
